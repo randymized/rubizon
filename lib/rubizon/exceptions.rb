5045d4307c1d4f97b5a237590dffd22413d228ef
randymized <ot40ddj02@sneakemail.com>
2010-12-17 10:24:02 -0600

    Add a set of exceptions.

---
+++ lib/rubizon/exceptions.rb
@@ -0,0 +1,10 @@
+module Rubizon
+  class RubizonError < StandardError; end
+  class UnsupportedSignatureMethodError < StandardError; end
+  class InvalidParameterError < RubizonError
+    attr_reader :message
+    def initialize(message='An invalid parameter is in the request')
+      @message= message
+    end
+  end
+end
