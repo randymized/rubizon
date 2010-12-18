1ec54825cc88d1eb649ef79ce7f498da4632076a
randymized <ot40ddj02@sneakemail.com>
2010-12-17 20:35:14 -0600

    Rename identifier.rb security_credentials.rb

--- lib/rubizon.rb
+++ lib/rubizon.rb
@@ -1,3 +1,3 @@
-require File.dirname(__FILE__) + "/rubizon/identifier"
+require File.dirname(__FILE__) + "/rubizon/security_credentials"
 require File.dirname(__FILE__) + "/rubizon/abstract_sig2_product"
 require File.dirname(__FILE__) + "/rubizon/request"
