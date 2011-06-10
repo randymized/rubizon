9b577705a51d5bb19c23af5ca81c7e00a2c56450
randymized <ot40ddj02@sneakemail.com>
2011-06-10 14:55:04 -0500

    Convert security_credentials test to spec

--- spec/security_credentials_spec.rb
+++ spec/security_credentials_spec.rb
@@ -1,32 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "SecurityCredentials" do
-    setup do
+    before do
       @id= Rubizon::SecurityCredentials.new(AWSAccessKeyId,SecretAccessKeyId)
       @arbitrary_string= 'x'
       @expected_signature= '6KClHg2k6AiXNRwaLa7sC7LIxP4NkUieZheem0eHnBI='
     end
     it "reports the access key ID" do
-      assert_equal AWSAccessKeyId, @id.accessID
+      @id.accessID.should == AWSAccessKeyId
     end
     it "won't allow accessing the secret access key ID" do
-      assert !@id.respond_to?(:secretID)
-      assert !@id.respond_to?(:secretKeyID)
-      assert !@id.respond_to?(:secretAccessKeyID)
-      assert !@id.respond_to?(:awsSecretAccessKeyID)
+      should_not respond_to :secretID, :secretKeyID, :secretAccessKeyID, :awsSecretAccessKeyID
     end
     it "signs an arbitrary string using the secretID" do
-      assert_equal @expected_signature, @id.sign256(@arbitrary_string)
+      @id.sign256(@arbitrary_string).should == @expected_signature
     end
     it "produces a different signature if initialized with a different access key" do
-      assert_not_equal @expected_signature, Rubizon::SecurityCredentials.new(AWSAccessKeyId,SecretAccessKeyId+'x').sign256(@arbitrary_string)
+      Rubizon::SecurityCredentials.new(AWSAccessKeyId,SecretAccessKeyId+'x').sign256(@arbitrary_string).should_not == @expected_signature
     end
     it "signs an arbitrary string based upon a signature method of HmacSHA256" do
-      assert_equal @expected_signature, @id.sign('HmacSHA256',@arbitrary_string)
+      @id.sign('HmacSHA256',@arbitrary_string).should == @expected_signature
     end
     it "raises an exception if an unsupported signature method is requested" do
-      assert_raise(Rubizon::UnsupportedSignatureMethodError) do
-        @id.sign('foo',@arbitrary_string)
-      end
+      lambda {@id.sign('foo',@arbitrary_string)}.should raise_error(Rubizon::UnsupportedSignatureMethodError)
     end
 end
