d8d139f92782b78f90754e9c3e6fe1f1ffabee04
randymized <ot40ddj02@sneakemail.com>
2011-06-10 15:12:48 -0500

    Convert sns test to spec.

--- spec/sns_spec.rb
+++ spec/sns_spec.rb
@@ -1,35 +1,37 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "SimpleNotificationService" do
-  @@access_key= '00000000000000000000'
-  @@credentials= Rubizon::SecurityCredentials.new(@@access_key,'1234567890')
-  @@arn= 'arn:aws:sns:us-east-1:123456789:My-Topic'
-  @@host= 'sns.us-east-1.amazonaws.com'
-  @@snsProduct= Rubizon::SimpleNotificationService.new(@@credentials,@@host)
+    before do
+      @access_key= '00000000000000000000'
+      @credentials= Rubizon::SecurityCredentials.new(@access_key,'1234567890')
+      @arn= 'arn:aws:sns:us-east-1:123456789:My-Topic'
+      @host= 'sns.us-east-1.amazonaws.com'
+      @snsProduct= Rubizon::SimpleNotificationService.new(@credentials,@host)
+    end
     it "formulates a url that will publish a message to a topic" do
       message= 'hello world'
-      req= @@snsProduct.topic(@@arn).publish(message)
-      assert_equal "http://#{@@host}/", req.endpoint
+      req= @snsProduct.topic(@arn).publish(message)
+      req.endpoint.should == "http://#{@host}/"
       q= CGI::parse(req.query_string)
-      assert_equal '2', q['SignatureVersion'].first
-      assert_equal 'HmacSHA256', q['SignatureMethod'].first
-      assert q['Signature'].first.is_a?(String)
-      assert_equal 44, CGI::unescape(q['Signature'].first).length
-      assert_equal @@access_key, q['AWSAccessKeyId'].first
-      assert q.has_key?('Timestamp')
-      assert_equal 'Publish', q['Action'].first
-      assert_equal @@arn, CGI::unescape(q['TopicArn'].first)
-      assert_equal message, CGI::unescape(q['Message'].first)
-      assert !q.has_key?('Subject')
+      q['SignatureVersion'].first.should == '2'
+      q['SignatureMethod'].first.should == 'HmacSHA256'
+      q['Signature'].first.should be_a(String)
+      CGI::unescape(q['Signature'].first).length.should == 44
+      q['AWSAccessKeyId'].first.should == @access_key
+      q.should have_key('Timestamp')
+      q['Action'].first.should == 'Publish'
+      CGI::unescape(q['TopicArn'].first).should == @arn
+      CGI::unescape(q['Message'].first).should == message
+      q.should_not have_key('Subject')
     end
     it "formulates a url that will publish a message and a subject to a topic" do
       message= 'world'
       subject= 'An important word'
-      req= @@snsProduct.topic(@@arn).publish(message,subject)
+      req= @snsProduct.topic(@arn).publish(message,subject)
       q= CGI::parse(req.query_string)
-      assert_equal 'Publish', q['Action'].first
-      assert_equal @@arn, CGI::unescape(q['TopicArn'].first)
-      assert_equal message, CGI::unescape(q['Message'].first)
-      assert_equal subject, CGI::unescape(q['Subject'].first)
+      q['Action'].first.should == 'Publish'
+      CGI::unescape(q['TopicArn'].first).should == @arn
+      CGI::unescape(q['Message'].first).should == message
+      CGI::unescape(q['Subject'].first).should == subject
     end
 end
