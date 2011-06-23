72fd8af89665f9d60c59776a314dd2e1945341b8
randymized <ot40ddj02@sneakemail.com>
2011-06-23 12:49:26 -0500

    Finish signature sample test conversion

--- test/test_signature_sample.rb
+++ test/test_signature_sample.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'helper'
 
 # This is a special test based upon a signature generation sample at
 # http://docs.amazonwebservices.com/AWSECommerceService/latest/DG/index.html?rest-signature.html
@@ -7,9 +7,8 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 # This also serves as a test of the integration of security credentials,
 # Actions, Products and Requests in order to generate a properly formed URL.
-
-describe "Signature generation sample" do
-    before do
+class TestSignatureGenerationSampe < Test::Unit::TestCase
+    def setup
       @credentials= Rubizon::SecurityCredentials.new('00000000000000000000','1234567890')
       @eCommerceServiceProduct= Rubizon::ProductAdvertisingProduct.new(@credentials)
       @eCommerceServiceRequestSubject= {
@@ -20,46 +19,46 @@ describe "Signature generation sample" do
       }
       @expectedSignature= 'Nace%2BU3Az4OhN7tISqgs1vdLBHBEijWcBeCqL5xN9xg%3D'
     end
-    it "calculates the signature (version 2) expected in the example" do
+    def test_calculates_the_version_2_signature_expected_in_the_example
       req= @eCommerceServiceProduct.item_lookup_request(@eCommerceServiceRequestSubject)
       q= req.query_string
-      req.canonical_querystring.should == <<____.rstrip
+      assert_equal <<____.rstrip, req.canonical_querystring
 AWSAccessKeyId=00000000000000000000&ItemId=0679722769&Operation=ItemLookup&ResponseGroup=ItemAttributes%2COffers%2CImages%2CReviews&Service=AWSECommerceService&Timestamp=2009-01-01T12%3A00%3A00Z&Version=2009-01-06
 ____
-      req.string_to_sign.should == <<____.rstrip
+      assert_equal <<____.rstrip, req.string_to_sign
 GET
 webservices.amazon.com
 /onca/xml
 AWSAccessKeyId=00000000000000000000&ItemId=0679722769&Operation=ItemLookup&ResponseGroup=ItemAttributes%2COffers%2CImages%2CReviews&Service=AWSECommerceService&Timestamp=2009-01-01T12%3A00%3A00Z&Version=2009-01-06
 ____
-      CGI::escape(CGI::parse(q)['Signature'].first).should == @expectedSignature
+      assert_equal @expectedSignature, CGI::escape(CGI::parse(q)['Signature'].first)
     end
-    it "Creates a URL that contains the expected host name for the sample request" do
+    def test_creates_a_URL_that_contains_the_expected_host_name_for_the_sample_request
       req= @eCommerceServiceProduct.item_lookup_request(@eCommerceServiceRequestSubject)
       uri = URI.parse(req.url);
-      uri.host.should == @eCommerceServiceProduct.host
+      assert_equal @eCommerceServiceProduct.host, uri.host
     end
-    it "Creates a URL that contains the expected path for the sample request" do
+    def test_creates_a_URL_that_contains_the_expected_path_for_the_sample_request
       req= @eCommerceServiceProduct.item_lookup_request(@eCommerceServiceRequestSubject)
       uri = URI.parse(req.url);
-      uri.path.should == @eCommerceServiceProduct.path
+      assert_equal @eCommerceServiceProduct.path, uri.path
     end
-    it "Creates a URL that contains the expected scheme for the sample request" do
+    def test_creates_a_URL_that_contains_the_expected_scheme_for_the_sample_request
       req= @eCommerceServiceProduct.item_lookup_request(@eCommerceServiceRequestSubject)
       uri = URI.parse(req.url);
-      uri.scheme.should == @eCommerceServiceProduct.scheme
+      assert_equal @eCommerceServiceProduct.scheme, uri.scheme
     end
-    it "Creates a URL that contains the expected query string for the sample request" do
+    def test_creates_a_URL_that_contains_the_expected_query_string_for_the_sample_request
       req= @eCommerceServiceProduct.item_lookup_request(@eCommerceServiceRequestSubject)
       uri = URI.parse(req.url);
       query= CGI::parse(uri.query)
       @eCommerceServiceRequestSubject.each do |k,v|
-        v.should == query.delete(k).first
+        assert_equal query.delete(k).first, v
       end
-      CGI::escape(query.delete('Operation').first).should == 'ItemLookup'
-      CGI::escape(query.delete('Service').first).should == 'AWSECommerceService'
-      query.delete('AWSAccessKeyId').first.should == @credentials.accessID
-      CGI::escape(query.delete('Signature').first).should == @expectedSignature
-      query.should be_empty   #there's nothing left!  All elements are accounted for
+      assert_equal 'ItemLookup',CGI::escape(query.delete('Operation').first)
+      assert_equal 'AWSECommerceService',CGI::escape(query.delete('Service').first)
+      assert_equal @credentials.accessID,query.delete('AWSAccessKeyId').first
+      assert_equal @expectedSignature,CGI::escape(query.delete('Signature').first)
+      assert query.empty?   #there's nothing left!  All elements are accounted for
     end
 end
