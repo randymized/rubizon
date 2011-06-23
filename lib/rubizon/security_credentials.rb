5c35893a628c2e88c0a9ba8b810375b3cff57927
randymized <ot40ddj02@sneakemail.com>
2011-06-23 09:16:49 -0500

    Cosmetic: improve code text alignment

--- lib/rubizon/security_credentials.rb
+++ lib/rubizon/security_credentials.rb
@@ -2,24 +2,24 @@ require 'hmac-sha2'
 require 'base64'
 require File.dirname(__FILE__) + '/exceptions'
 module Rubizon
-    class SecurityCredentials
-      attr_reader :accessID
-      def initialize(accessID, secretID)
-        @accessID= accessID
-        @secretID= secretID
-      end
-      def sign256(string_to_sign)
-        @hmac256||= HMAC::SHA256.new(@secretID)
-        @hmac256.update(string_to_sign)
-        Base64.encode64(@hmac256.digest).chomp
-      end
-      def sign(signature_method,string_to_sign)
-        case signature_method
-          when 'HmacSHA256'
-            sign256(string_to_sign)
-          else
-            raise UnsupportedSignatureMethodError, "The #{signature_method} signature method is not supported"
-          end
-      end
+  class SecurityCredentials
+    attr_reader :accessID
+    def initialize(accessID, secretID)
+      @accessID= accessID
+      @secretID= secretID
     end
+    def sign256(string_to_sign)
+      @hmac256||= HMAC::SHA256.new(@secretID)
+      @hmac256.update(string_to_sign)
+      Base64.encode64(@hmac256.digest).chomp
+    end
+    def sign(signature_method,string_to_sign)
+      case signature_method
+        when 'HmacSHA256'
+          sign256(string_to_sign)
+        else
+          raise UnsupportedSignatureMethodError, "The #{signature_method} signature method is not supported"
+        end
+    end
+  end
 end
