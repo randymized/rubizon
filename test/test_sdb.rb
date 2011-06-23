7f769046138912a592d1c4773f01ba281f2d8f15
randymized <ot40ddj02@sneakemail.com>
2011-06-23 14:32:36 -0500

    Skip tests that are currently just placeholders.

--- test/test_sdb.rb
+++ test/test_sdb.rb
@@ -28,6 +28,7 @@ class TestSimpleDB < Test::Unit::TestCase
     assert !r.include?(TestDomainName)
   end
   def test_deletes_a_domain
+    skip
   end
   def test_lists_domains
     r= @sdb.list_domains.request
@@ -36,25 +37,36 @@ class TestSimpleDB < Test::Unit::TestCase
     assert r.meta.key?(:RequestId)
   end
   def test_gets_a_domains_metadata
+    skip
   end
   def test_creates_an_item
+    skip
   end
   def test_adds_attributes_to_an_item
+    skip
   end
   def test_replaces_attributes_of_an_item
+    skip
   end
   def test_both_adds_and_replaces_attributes_of_an_item
+    skip
   end
   def test_adds_attributes_to_an_item_and_checks_that_an_existing_attribute_has_not_changed_in_the_interim
+    skip
   end
   def test_tries_to_add_attributes_to_an_item_but_fails_because_one_of_the_attributes_has_changed
+    skip
   end
   def test_deletes_some_attributes_of_an_item
+    skip
   end
   def test_deletes_all_attributes_of_an_item
+    skip
   end
   def test_adds_attributes_to_several_items_in_one_database_request
+    skip
   end
   def test_uses_a_select_expression_to_retrive_some_items
+    skip
   end
 end
