67e0f413a9f9c662882978ad28c0cb8170364113
randymized <ot40ddj02@sneakemail.com>
2011-06-23 12:13:02 -0500

    Convert security credentials test.

--- test/test_security_credentials.rb
+++ test/test_security_credentials.rb
@@ -1,27 +1,32 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'helper'
 
-describe "SecurityCredentials" do
-    before do
+class TestSecurityCredentials < Test::Unit::TestCase
+    def setup
       @id= Rubizon::SecurityCredentials.new(ExampleAWSAccessKeyId,ExampleSecretAccessKeyId)
       @arbitrary_string= 'x'
       @expected_signature= '6KClHg2k6AiXNRwaLa7sC7LIxP4NkUieZheem0eHnBI='
     end
-    it "reports the access key ID" do
-      @id.accessID.should == ExampleAWSAccessKeyId
+    def test_reports_the_access_key_ID
+      assert_equal(ExampleAWSAccessKeyId, @id.accessID)
     end
-    it "won't allow accessing the secret access key ID" do
-      should_not respond_to :secretID, :secretKeyID, :secretAccessKeyID, :awsSecretAccessKeyID
+    def test_wont_allow_accessing_the_secret_access_key_ID
+      assert !@id.respond_to?(:secretID)
+      assert !@id.respond_to?(:secretKeyID)
+      assert !@id.respond_to?(:secretAccessKeyID)
+      assert !@id.respond_to?(:awsSecretAccessKeyID)
     end
-    it "signs an arbitrary string using the secretID" do
-      @id.sign256(@arbitrary_string).should == @expected_signature
+    def test_signs_an_arbitrary_string_using_the_secretID
+      assert_equal(@expected_signature, @id.sign256(@arbitrary_string))
     end
-    it "produces a different signature if initialized with a different access key" do
-      Rubizon::SecurityCredentials.new(ExampleAWSAccessKeyId,ExampleSecretAccessKeyId+'x').sign256(@arbitrary_string).should_not == @expected_signature
+    def test_produces_a_different_signature_if_initialized_with_a_different_access_key
+      assert_not_equal(@expected_signature, Rubizon::SecurityCredentials.new(ExampleAWSAccessKeyId,ExampleSecretAccessKeyId+'x').sign256(@arbitrary_string))
     end
-    it "signs an arbitrary string based upon a signature method of HmacSHA256" do
-      @id.sign('HmacSHA256',@arbitrary_string).should == @expected_signature
+    def test_signs_an_arbitrary_string_based_upon_a_signature_method_of_HmacSHA256
+      assert_equal(@expected_signature, @id.sign('HmacSHA256',@arbitrary_string))
     end
-    it "raises an exception if an unsupported signature method is requested" do
-      lambda {@id.sign('foo',@arbitrary_string)}.should raise_error(Rubizon::UnsupportedSignatureMethodError)
+    def test_raises_an_exception_if_an_unsupported_signature_method_is_requested
+      assert_raise(Rubizon::UnsupportedSignatureMethodError) do
+        @id.sign('foo',@arbitrary_string)
+      end
     end
 end
