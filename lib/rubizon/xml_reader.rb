b859e63341705590f73b6e325756797019610374
randymized <ot40ddj02@sneakemail.com>
2011-06-23 09:45:43 -0500

    Change XMLNode to XMLReader::Node

--- lib/rubizon/xml_reader.rb
+++ lib/rubizon/xml_reader.rb
@@ -1,30 +1,5 @@
 require 'libxml'
 module Rubizon
-  
-  # Represents a single XML node, such as the start or end of an element.
-  class XMLNode
-    def initialize(reader)
-      @reader= reader
-    end
-    # The name of an node will be that of the tag with an optional prefix.  
-    # If this is an opening tag, the name will be prefixed by '<'.
-    # If this is a closing tag, the name will be prefixed by '/'.
-    def name
-      @name ||=
-        if @reader.node_type == LibXML::XML::Reader::TYPE_ELEMENT
-          '<'+@reader.name
-        elsif @reader.node_type == LibXML::XML::Reader::TYPE_END_ELEMENT
-          '/'+@reader.name
-        else
-          @reader.name
-        end
-    end
-    # The value of a leaf element, the text between the opening and closing tags.
-    def value
-      @reader.read_string
-    end
-  end
-  
   # Reads the nodes of XML.
   # Currently this is a wrapper around LibXML::XML::Reader
   class XMLReader
@@ -33,7 +8,31 @@ module Rubizon
     end
     def each
       while @reader.read
-        yield XMLNode.new(@reader)
+        yield Node.new(@reader)
+      end
+    end
+
+    # Represents a single XML node, such as the start or end of an element.
+    class Node
+      def initialize(reader)
+        @reader= reader
+      end
+      # The name of an node will be that of the tag with an optional prefix.  
+      # If this is an opening tag, the name will be prefixed by '<'.
+      # If this is a closing tag, the name will be prefixed by '/'.
+      def name
+        @name ||=
+          if @reader.node_type == LibXML::XML::Reader::TYPE_ELEMENT
+            '<'+@reader.name
+          elsif @reader.node_type == LibXML::XML::Reader::TYPE_END_ELEMENT
+            '/'+@reader.name
+          else
+            @reader.name
+          end
+      end
+      # The value of a leaf element, the text between the opening and closing tags.
+      def value
+        @reader.read_string
       end
     end
   end
