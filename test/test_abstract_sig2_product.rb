125dfef72549a3c10e90c872d89f9750c27c98ad
randymized <ot40ddj02@sneakemail.com>
2011-06-23 12:41:13 -0500

    Convert AbstractSig2Product test

--- test/test_abstract_sig2_product.rb
+++ test/test_abstract_sig2_product.rb
@@ -1,62 +1,62 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'helper'
 
-describe "AbstractSig2Product" do
-    before do
+class TestAbstractSig2Product < Test::Unit::TestCase
+    def setup
       @region= 'us-east-1'
       @product= 'sns'
       @arn= "arn:aws:#{@product}:#{@region}:123456789:My-Topic" #arn:aws:sns:us-east-1:123456789:My-Topic
     end
-    it "calculates the hostname if only an ARN is specified" do
+    def test_calculates_the_hostname_if_only_an_ARN_is_specified
       # If the ARN is: arn:aws:sns:us-east-1:123456789:My-Topic
       # the host would be: sns.us-east-1.amazonaws.com
       # The host may be specified specifically if it cannot be deduced from the ARN in this way
       prod= Rubizon::AbstractSig2Product.new(:arn=>@arn)
-      prod.host.should == "#{@product}.#{@region}.amazonaws.com"
+      assert_equal "#{@product}.#{@region}.amazonaws.com", prod.host
     end
-    it "returns any specific host name, even if a different one might be calculated from a specified ARN" do
+    def test_returns_any_specific_host_name_even_if_a_different_one_might_be_calculated_from_a_specified_ARN
       prod= Rubizon::AbstractSig2Product.new(:arn=>@arn, :host=>'foo.com')
-      prod.host.should == 'foo.com'
+      assert_equal 'foo.com', prod.host
     end
-    it "determines the host if given a URL" do
+    def test_determines_the_host_if_given_a_URL
       prod= Rubizon::AbstractSig2Product.new(:url=>'http://foo.com/')
-      prod.host.should == 'foo.com'
+      assert_equal 'foo.com', prod.host
     end
-    it "determines the scheme if given a URL" do
+    def test_determines_the_scheme_if_given_a_URL
       prod= Rubizon::AbstractSig2Product.new(:url=>'ftp://foo.com/')
-      prod.scheme.should == 'ftp'
+      assert_equal 'ftp', prod.scheme
     end
-    it "determines the path if given a URL" do
+    def test_determines_the_path_if_given_a_URL
       prod= Rubizon::AbstractSig2Product.new(:url=>'http://foo.com/bar')
-      prod.path.should == '/bar'
+      assert_equal '/bar', prod.path
     end
-    it "calculates the URL if only an ARN is specified" do
+    def test_calculates_the_URL_if_only_an_ARN_is_specified
       # If the ARN is: arn:aws:sns:us-east-1:123456789:My-Topic
       # the URL would be: https://sns.us-east-1.amazonaws.com/
       # The host may be specified specifically if it cannot be deduced from the ARN in this way
       prod= Rubizon::AbstractSig2Product.new(:arn=>@arn)
-      prod.endpoint.should == "https://#{@product}.#{@region}.amazonaws.com/"
+      assert_equal "https://#{@product}.#{@region}.amazonaws.com/", prod.endpoint
     end
-    it "calculates the URL if only an ARN and a scheme is specified" do
+    def test_calculates_the_URL_if_only_an_ARN_and_a_scheme_is_specified
       prod= Rubizon::AbstractSig2Product.new(:arn=>@arn,:scheme=>:http)
-      prod.endpoint.should == "http://#{@product}.#{@region}.amazonaws.com/"
+      assert_equal "http://#{@product}.#{@region}.amazonaws.com/", prod.endpoint
     end
-    it "calculates the URL if only host is specified" do
+    def test_calculates_the_URL_if_only_host_is_specified
       prod= Rubizon::AbstractSig2Product.new(:host=>'example.com')
-      prod.endpoint.should == "https://example.com/"
+      assert_equal "https://example.com/", prod.endpoint
     end
-    it "prefers a specified host over one built from ARN" do
+    def test_prefers_a_specified_host_over_one_built_from_ARN
       prod= Rubizon::AbstractSig2Product.new(:host=>'foo.com',:scheme=>'http',:arn=>@arn)
-      prod.endpoint.should == 'http://foo.com/'
+      assert_equal 'http://foo.com/', prod.endpoint
     end
-    it "returns any specified path" do
+    def test_returns_any_specified_path
       prod= Rubizon::AbstractSig2Product.new(:host=>'x.com',:path=>'abc/xyz')
-      prod.path.should == 'abc/xyz'
+      assert_equal 'abc/xyz', prod.path
     end
-    it "appends any specified path to a URL generated from an ARN" do
+    def test_appends_any_specified_path_to_a_URL_generated_from_an_ARN
       prod= Rubizon::AbstractSig2Product.new(:arn=>@arn, :path=>'/abc/xyz')
-      prod.endpoint.should == "https://#{@product}.#{@region}.amazonaws.com/abc/xyz"
+      assert_equal "https://#{@product}.#{@region}.amazonaws.com/abc/xyz", prod.endpoint
     end
-    it "returns query elements not related to specifying the endpoint in the query_elements hash" do
+    def test_returns_query_elements_not_related_to_specifying_the_endpoint_in_the_query_elements_hash
       prod= Rubizon::AbstractSig2Product.new(
         'arn'=>@arn, 
         'scheme'=>'http',
@@ -64,10 +64,10 @@ describe "AbstractSig2Product" do
         'path'=>'/abc/xyz',
         'foo'=>'bar'
       )
-      prod.query_elements.size.should == 4
-      prod.query_elements['foo'].should == 'bar'
-      prod.query_elements['SignatureMethod'].should == 'HmacSHA256'
-      prod.query_elements['SignatureVersion'].should == 2
+      assert_equal 4, prod.query_elements.size
+      assert_equal 'bar',prod.query_elements['foo']
+      assert_equal 'HmacSHA256',prod.query_elements['SignatureMethod']
+      assert_equal 2,prod.query_elements['SignatureVersion']
     end
 end
 
